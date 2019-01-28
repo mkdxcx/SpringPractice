887387f6b6a172f79bd793ab928f1ee53b429994
CORP\MKRZESIN <maciej.krzesinski@capgemini.com>
2019-01-28 08:08:17 +0100

    Merge branch 'develop_ownConfiguration' of https://github.com/mkdxcx/SpringPractice into develop_ownConfiguration
    # Conflicts:
    #	src/main/java/com/mkrzesi/springpractice/soundsystem/StartMeUp.java
    #	src/main/java/com/mkrzesi/springpractice/soundsystem/configuration/CDplayerConfiguration.java
    #	src/test/java/com/mkrzesi/springpractice/soundsystem/CDplayerTest.java

--- src/main/java/com/mkrzesi/springpractice/knight/configuration/KnightConfiguration.java
+++ src/main/java/com/mkrzesi/springpractice/knight/configuration/KnightConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KnightConfiguration {
 
-    @Bean
+    @Bean(name="braveKnight")
     public Knight braveKnight() {
         return new BraveKnight(braveQuest());
     }
