119ce300477059d964627cf14a9198405a658395
tomasz.lis <S@m8un6$>
2019-02-02 12:46:10 +0100

    test structure changed to spring runner

--- src/test/java/com/mkrzesi/springpractice/knight/KnightTest.java
+++ src/test/java/com/mkrzesi/springpractice/knight/KnightTest.java
@@ -1,16 +1,41 @@
 package com.mkrzesi.springpractice.knight;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.mock;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class KnightTest {
+
+    @Autowired
+    @Qualifier("braveKnightOne")
+    private BraveKnight braveKnightOne;
+
+    @Autowired
+    @Qualifier("braveKnightTwo")
+    private BraveKnight braveKnightTwo;
+
+    @Autowired
+    private CowardKnight cowardKnight;
+
 
     @Test
     public void checkIfDragonIsKilled(){
-        Quest mockQuest = mock(Quest.class);
+        braveKnightOne.makeQuest();
+    }
 
-        BraveKnight braveKnight = new BraveKnight(mockQuest);
-        braveKnight.makeQuest();
+    @Test
+    public void checkIfDamselIsSave(){
+        braveKnightTwo.makeQuest();
+    }
+
+    @Test
+    public void checkIfCowardKnightIsPassive(){
+        cowardKnight.makeQuest();
     }
 }
