dd09570224bbe056c15d2d1988f8f0f5abd700fe
tomasz.lis <S@m8un6$>
2019-02-02 12:44:45 +0100

    negligible  annotations removed

--- src/main/java/com/mkrzesi/springpractice/knight/aspects/Minstrel.java
+++ src/main/java/com/mkrzesi/springpractice/knight/aspects/Minstrel.java
@@ -1,22 +1,16 @@
 package com.mkrzesi.springpractice.knight.aspects;
 
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Aspect
 public class Minstrel {
 
-    Logger logger = LoggerFactory.getLogger(getClass());
+    private static final Logger logger = LoggerFactory.getLogger(Minstrel.class);
 
-    @Before("execution(* makeQuest(..))")
     public void singBeforeQuest(){
         logger.info("Minstrel is singing BEFORE quest");
     }
 
-    @After("execution(* makeQuest(..))")
     public void singAfterQuest(){
         logger.info("Minstrel is singing AFTER quest");
     }
