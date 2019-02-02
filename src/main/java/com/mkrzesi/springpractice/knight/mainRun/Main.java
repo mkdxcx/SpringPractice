6bac0c4c33af7ed0f779e3fe62fcb7c639aef703
tomasz.lis <S@m8un6$>
2019-02-02 21:25:54 +0100

    fix on Main Class

--- src/main/java/com/mkrzesi/springpractice/knight/mainRun/Main.java
+++ src/main/java/com/mkrzesi/springpractice/knight/mainRun/Main.java
@@ -1,16 +1,19 @@
 package com.mkrzesi.springpractice.knight.mainRun;
 
+import com.mkrzesi.springpractice.SpringpracticeApplication;
 import com.mkrzesi.springpractice.knight.BraveKnight;
 import com.mkrzesi.springpractice.knight.CowardKnight;
 import com.mkrzesi.springpractice.knight.Knight;
 import com.mkrzesi.springpractice.knight.aspects.Minstrel;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new FileSystemXmlApplicationContext("src/main/java/com/mkrzesi/springpractice/knight/configuration/knights.xml");
+        ApplicationContext context = SpringApplication.run(SpringpracticeApplication.class, args);
+                //new FileSystemXmlApplicationContext("src/main/resources/knight/knights.xml");
 
         Knight braveKnightOne = context.getBean("braveKnightOne", BraveKnight.class);
         braveKnightOne.makeQuest();
