1c815d773521664597eeee1d02a13d993127a645
CORP\MKRZESIN <maciej.krzesinski@capgemini.com>
2019-04-03 11:18:04 +0200

    Java aspects configuration WiP.

--- src/main/java/com/mkrzesi/springpractice/knight/aspects/Minstrel.java
+++ src/main/java/com/mkrzesi/springpractice/knight/aspects/Minstrel.java
@@ -2,6 +2,7 @@ package com.mkrzesi.springpractice.knight.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,12 +11,15 @@ public class Minstrel {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Before("execution(public void makeQuest())")
-    public void singBeforeQuest(){
+    @Pointcut("execution(*com.mkrzesi.springpractice.knight.makeQuest())")
+    public void sing(){}
+
+    @Before("sing()")
+    public void singBeforeQuestAdvice(){
         logger.info("Minstrel is singing BEFORE quest");
     }
 
-    public void singAfterQuest(){
+    public void singAfterQuestAdvice(){
         logger.info("Minstrel is singing AFTER quest");
     }
 }
--- src/main/java/com/mkrzesi/springpractice/knight/configuration/Main.java
+++ src/main/java/com/mkrzesi/springpractice/knight/configuration/Main.java
@@ -3,6 +3,7 @@ package com.mkrzesi.springpractice.knight.configuration;
 import com.mkrzesi.springpractice.knight.BraveKnight;
 import com.mkrzesi.springpractice.knight.CowardKnight;
 import com.mkrzesi.springpractice.knight.Knight;
+import com.mkrzesi.springpractice.knight.aspects.Minstrel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -10,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
 
         ApplicationContext context = new AnnotationConfigApplicationContext("com\\mkrzesi\\springpractice\\knight\\configuration\\");
+
         Knight braveKnightOne = context.getBean("braveKnightOne",BraveKnight.class);
         braveKnightOne.makeQuest();
 
