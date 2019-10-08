1d1c8098f056bf3973d28bccb8c6ab3b2f037ecf
CORP\MKRZESIN <maciej.krzesinski@capgemini.com>
2019-10-08 19:35:06 +0200

    Aspect - WiP

--- src/main/java/com/mkrzesi/springpractice/soundsystem/apect/VolumeController.java
+++ src/main/java/com/mkrzesi/springpractice/soundsystem/apect/VolumeController.java
@@ -3,6 +3,7 @@ package com.mkrzesi.springpractice.soundsystem.apect;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +12,15 @@ public class VolumeController {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Before("execution(* play(..))")
+    @Pointcut("execution(* com.mkrzesi.springpractice.soundsystem.business.*.*.*())")
+    public void changeVolume(){}
+
+    @Before("changeVolume()")
     public void volumeUp(){
         logger.info("Volume Controller - Volume UP");
     }
 
-    @After("execution(* play(..))")
+    @After("changeVolume()")
     public void volumeDown(){
         logger.info("Volume Controller - Volume DOWN");
     }
